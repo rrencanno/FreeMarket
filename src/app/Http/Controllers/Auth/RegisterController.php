dfe2fa61e635a5e423cc1f8abd54509aa6609548
Harada Koudai <rrencanno@gmail.com>
2025-03-31 05:59:48 +0900

    2025_03_31_6:00_save

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\RegisterStep2Request;
 use App\Models\User;
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,18 +11,17 @@ Route::get('/register/step1', [RegisterController::class, 'showRegistrationForm'
 Route::post('/register/step1', [RegisterController::class, 'register']);
 Route::get('/register/step2', [RegisterController::class, 'create'])->name('register.step2');
 Route::post('/register/step2', [RegisterController::class, 'store'])->name('register.step2.store');
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.attempt');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 Route::get('/weight_logs', [WeightLogController::class, 'index'])->name('weight_logs.index');
-
 Route::post('/weight_logs', [WeightLogController::class, 'store'])->name('weight_logs.store');
-
 Route::get('/weight_logs/{weightLogId}/edit', [WeightLogController::class, 'edit'])->name('weight_logs.edit');
 Route::put('/weight_logs/{weightLogId}', [WeightLogController::class, 'update'])->name('weight_logs.update');
 Route::delete('/weight_logs/{weightLogId}', [WeightLogController::class, 'destroy'])->name('weight_logs.destroy');
 Route::get('/weight_logs/target_setting', [WeightLogController::class, 'editTargetWeight'])
     ->name('weight_logs.target_setting');
-
 Route::post('/weight_logs/target_setting', [WeightLogController::class, 'updateTargetWeight'])
     ->name('weight_logs.target_setting.update');
